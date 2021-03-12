5a64c1a493ba15303527e3efeb5374dde7ce9818
cinemaFactoryAdmin <78069295+cinemaFactoryAdmin@users.noreply.github.com>
2021-03-12 20:45:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -56,7 +56,7 @@ Bot will automatically search for your files and give links to that!
 <b>© @TroJanzHEX</b>"""
 
 
-    ABOUTt_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
 ⭕️<b>Creater :</b> @TroJanzHEX
 
